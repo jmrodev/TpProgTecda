27ba69d85acbded9997166f84a84ba271d78e268
jmrodev <juanmarcelo.rodrigueztandil@gmail.com>
2024-08-24 22:42:18 -0300

    Refactorizar generación de ID de usuario
    Este commit refactoriza la función `userIdGeneratedByUser` para mejorar la generación de IDs de usuario. Se han creado variables separadas para diferentes tipos de caracteres, como números, letras mayúsculas, letras minúsculas y signos. Además, se ha agregado una opción para que el usuario elija el tipo de caracteres que desea utilizar. Esto mejora la flexibilidad y personalización de la generación de IDs de usuario.

--- UserIdGeneratedByUser.js
+++ UserIdGeneratedByUser.js
@@ -26,16 +26,50 @@ function generateString(countChar, char, length) {
   return string;
 }
 
-function print(data){
+function print(data) {
   return console.log(data + '\n');
 }
 
 function userIdGeneratedByUser() {
-  let char = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
-  // let char = "0987654321"
+  let generalChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+  let numberChar = "0987654321"
+  let letterChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+  let upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  let lowerChar = "abcdefghijklmnopqrstuvwxyz"
+  let signChar = "\|@#~½¬{[]}\][{} ̣·•ª!·$%&/()=?¿*^Ç¨_:;><,.-ḉ+`¡'º"
+
+  let selectChar = prompt('Ingresa el tipo de caracteres que deseas usar : \n 1 - Números \n 2 - Letras \n 3 - Mayúsculas \n 4 - Minúsculas \n 5 - Signos \n 6 - General \n : ');
+
+  let char = "";
+
+  switch (selectChar) {
+    case '1':
+      char = numberChar;
+      break;
+    case '2':
+      char = letterChar;
+      break;
+    case '3':
+      char = upperChar;
+      break;
+    case '4':
+      char = lowerChar;
+      break;
+    case '5':
+      char = signChar;
+      break;
+    case '6':
+      char = generalChar;
+      break;
+    default:
+      char = generalChar;
+      break;
+  }
   let length = char.length;
+  
   let countChar = prompt('Ingresa la cantidad de caracteres : ');
   let countId = prompt('Ingresa la cantidad de Ids : ')
+
   print(generateId(countId, countChar, char, length));
 }
 
