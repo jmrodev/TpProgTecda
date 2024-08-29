f94f67fe7e52a5fb8a575f8fddfe3a09b4eb79e1
Juan Marcelo Rodriguez <juanmrodriguez@alumnos.exa.unicen.edu.ar>
2024-08-28 22:06:18 -0300

    tratando de que funcione

--- competenciaatletica.js
+++ competenciaatletica.js
@@ -1,76 +1,47 @@
 let atletas = [
-    {
-        "nombre": "Isabella",
-        "tiempo": 30
-    },
-    {
-        "nombre": "Valentina",
-        "tiempo": 49
-    },
-    {
-        "nombre": "Sofía",
-        "tiempo": 60
-    },
-    {
-        "nombre": "Camila",
-        "tiempo": 50
-    },
-    {
-        "nombre": "Natalia",
-        "tiempo": 47
-    },
-    {
-        "nombre": "Emilia",
-        "tiempo": 11
-    },
-    {
-        "nombre": "Gabriela",
-        "tiempo": 22
-    },
-    {
-        "nombre": "Lucía",
-        "tiempo": 53
-    },
-    {
-        "nombre": "Victoria",
-        "tiempo": 48
-    },
-    {
-        "nombre": "María",
-        "tiempo": 10
-    }
+    { "nombre": "Isabella", "tiempo": 30 },
+    { "nombre": "Valentina", "tiempo": 49 },
+    { "nombre": "Sofía", "tiempo": 60 },
+    { "nombre": "Camila", "tiempo": 50 },
+    { "nombre": "Natalia", "tiempo": 47 },
+    { "nombre": "Emilia", "tiempo": 11 },
+    { "nombre": "Gabriela", "tiempo": 22 },
+    { "nombre": "Lucía", "tiempo": 53 },
+    { "nombre": "Victoria", "tiempo": 10 },
+    { "nombre": "María", "tiempo": 10 }
 ];
 
-
-
-
-
-
-function getPerson(person) {
-   let winers = []
+function getPerson(person, position = 1) {
+    let persons = [];
     for (let index = 0; index < atletas.length; index++) {
         if (person.tiempo === atletas[index].tiempo) {
-            winers.push(atletas[index])
-            return winers
+            persons.push(atletas[index]);
         }
     }
+    return persons[position - 1]; // Devuelve el atleta en la posición indicada
 }
 
 function getMinorTime(array, prop) {
-    let minor = array[0]
+    let minor = [array[0], null]; // `minor[0]` es el menor tiempo, `minor[1]` es el segundo menor
+
     for (let index = 1; index < array.length; index++) {
-        if (minor.tiempo > array[index].tiempo) {
-            minor = array[index]
-            return minor
+        if (array[index][prop] < minor[0][prop]) {
+            // Si encontramos un nuevo menor, movemos el antiguo menor a minor[1]
+            minor[1] = minor[0];
+            minor[0] = array[index];
+        } else if (minor[1] === null || array[index][prop] < minor[1][prop]) {
+            // Si no es menor que minor[0], pero es menor que minor[1] o minor[1] es null, lo asignamos a minor[1]
+            minor[1] = array[index];
         }
     }
+    return minor;
 }
 
 function getWinner() {
-    let minorTime = getMinorTime(atletas, 'tiempo')
-   let win =  getPerson(minorTime)
-   console.log(win);
-   
+    let [firstPlace, secondPlace] = getMinorTime(atletas, 'tiempo');
+
+    console.log("Primer lugar:", firstPlace);
+    console.log("Segundo lugar:", secondPlace);
 }
 
-getWinner()
+getWinner();
